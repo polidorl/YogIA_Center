bcda24b3a331486a6fab21a49b7058a79615ef0d
polidorl <polidor.lisbeth4@gmail.com>
2026-02-23 08:20:26 -0400

    Configuración inteligente para Vercel y GitHub

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+// Detecta si el "robot" de GitHub está trabajando
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig = {
   output: 'export',
-  basePath: '/YogIA_Center', // <--- AÑADE ESTA LÍNEA (Debe coincidir con el nombre de tu repositorio)
+  // Si es GitHub, usa la subcarpeta. Si es Vercel, usa la raíz.
+  basePath: isGithubActions ? '/YogIA_Center' : '', 
   images: {
     unoptimized: true,
   },
